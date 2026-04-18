bec4cbef736ccd6347e3265ecdb849e6627b6034
ar-sayeem <adnan.rahman.sayeem@gmail.com>
2026-04-18 15:00:14 +0600

    add: Building Header, Banner, Logo

--- src/layout/app-layout.jsx
+++ src/layout/app-layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../components/header";
 
 const AppLayout = () => {
   return (
     <div className="bg-gray-950 text-white min-h-screen">
       <div className="container px-6 py-4 mx-auto">
-        {/* header */}
+        <Header />
 
         <main>
           <Outlet />
@@ -17,4 +18,3 @@ const AppLayout = () => {
 
 export default AppLayout;
 
-// Next-> 00:14:46 Building Header
